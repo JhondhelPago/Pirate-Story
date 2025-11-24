79e1dc32926a81481b0be408605127ce5f8d7692
JhondhelPago <126649267+JhondhelPago@users.noreply.github.com>
2025-11-24 09:58:45 +0800

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,65 @@
+import '@esotericsoftware/spine-pixi-v8';
+
+import { Application } from 'pixi.js';
+import { initAssets } from './utils/assets';
+import { navigation } from './utils/navigation';
+import { PirateLoadScreen } from './screens/PirateLoad';
+import { TiledBackground } from './ui/TiledBackground';
+import { sound } from '@pixi/sound';
+
+export const app = new Application();
+
+function resize() {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const minWidth = 375;
+    const minHeight = 700;
+
+    const scaleX = windowWidth < minWidth ? minWidth / windowWidth : 1;
+    const scaleY = windowHeight < minHeight ? minHeight / windowHeight : 1;
+    const scale = Math.max(scaleX, scaleY);
+
+    const width = windowWidth * scale;
+    const height = windowHeight * scale;
+
+    app.renderer.canvas.style.width = `${windowWidth}px`;
+    app.renderer.canvas.style.height = `${windowHeight}px`;
+
+    window.scrollTo(0, 0);
+    app.renderer.resize(width, height);
+    navigation.resize(width, height);
+}
+
+function visibilityChange() {
+    if (document.hidden) {
+        sound.pauseAll();
+        navigation.blur();
+    } else {
+        sound.resumeAll();
+        navigation.focus();
+    }
+}
+
+async function init() {
+    await app.init({
+        resolution: Math.max(window.devicePixelRatio, 2),
+        backgroundColor: 0xffffff,
+    });
+
+    document.body.appendChild(app.canvas);
+
+    window.addEventListener('resize', resize);
+    resize();
+
+    document.addEventListener('visibilitychange', visibilityChange);
+
+    await initAssets();
+
+    // Persistent background
+    navigation.setBackground(TiledBackground);
+
+    // ⭐⭐ ALWAYS show PirateLoadScreen — no conditions
+    await navigation.showScreen(PirateLoadScreen);
+}
+
+init();
---
+++ src/ui/pirate/Background.ts
@@ -0,0 +1,15 @@
+import { Container, Sprite } from 'pixi.js';
+import gsap from 'gsap';
+
+
+
+export class Background extends Container {
+    private image: Sprite;
+
+    constructor(){
+        super();
+        this.scale
+    }
+
+    
+}
